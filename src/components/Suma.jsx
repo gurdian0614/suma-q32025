3ce7d2b5c0e44f151ccf13b91b99a403d7d0b904
gurdian0614 <ivan.gurdian@outlook.com>
2025-08-07 20:19:07 -0600

    Building component

--- src/components/Suma.jsx
+++ src/components/Suma.jsx
@@ -1,7 +1,47 @@
+import UseSuma from "../hooks/UseSuma";
 
 const Suma = () => {
+    const {
+        numero1,
+        setNumero1,
+        numero2,
+        setNumero2,
+        resultado,
+        Suma,
+        Limpiar,
+    } = UseSuma();
+
     return (
-        <label className="form-label">Hola Mundo</label>
+        <div className="container mt-5">
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Ingrese primer número:</label>
+                    <input type="number" className="form-control" onChange={(e) => setNumero1(e.target.value)} />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Ingrese segundo número:</label>
+                    <input type="number" className="form-control" onChange={(e) => setNumero2(e.target.value)} />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <label className="form-label">Resultado:</label>
+                    <input className="form-control" value={resultado} readOnly />
+                </div>
+            </div>
+
+            <div className="row justify-content-center">
+                <div className="col-6 mb-3">
+                    <button className="btn btn-success" onClick={Suma}>Sumar</button>
+                    
+                    <button className="btn btn-danger" onClick={Limpiar}>Limpiar</button>
+                </div>
+            </div>
+        </div>
     )
 };
 
